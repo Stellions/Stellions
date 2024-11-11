5164d86f58b7fb80d577127308a388f0cd7ef10c
Stellions <sidereal.skiess@gmail.com>
2024-11-11 13:19:11 -0600

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -26,8 +26,7 @@ let options = {
     /* --------------------------------------------------------------- */
     index: {
 
-        promptSheetPage: sheetPages.prompts,
-        numOfPrompts: 3,
+       
 
         staffSheetPage: sheetPages.staff,
         numOfStaff: 8,
